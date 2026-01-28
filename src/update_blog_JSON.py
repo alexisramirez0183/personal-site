87e9d97be23adb87ca280455d0831191f7c54a91
Alexis Ramirez <alexisramirez0183@gmail.com>
2026-01-28 14:06:45 -0500

    git updating close

--- src/update_blog_JSON.py
+++ src/update_blog_JSON.py
@@ -1,16 +1,19 @@
 import os
 import json
+from bs4 import BeautifulSoup
 
 filepath = './blog_posts/'
 
 filenames = os.listdir(filepath)
 
-print(f"names in Images Folder", filenames) 
+print(f"names in Blog Folder", filenames)
 
-json_file = "./blog_posts.json"
+# json_file = "./blog_posts.json"
 
-with open (json_file,'w') as file:
-    json.dump(filenames, file, indent=4)
+# with open (json_file,'w') as file:
+#     json.dump(filenames, file, indent=4)
+
+
 
 
 
