3666a47a58adb101406ccfe3db1925c62fd563c6
alexisramirez0183 <alexisramirez0183@gmail.com>
2026-01-29 14:48:28 -0500

    js

--- script.js
+++ script.js
@@ -114,9 +114,10 @@ document.addEventListener("DOMContentLoaded",() =>{
         fetch("blog_posts.json?nocache="+ Date.now())
         .then(res => res.json())
         .then(obj => {
+            data = obj;
             const myLink = document.createElement("a");
-            console.log("file path objects log", obj)
-            const postTitle = obj.post_title
+            console.log("file path objects log", data)
+            const postTitle = data.post_title
             console.log(postTitle)
         })
     };
